0df6f551c105d5b047e0c16621bdeb80c50e8661
Craig R Webster <craig@barkingiguana.com>
2008-01-18 08:54:44 +0000

    Explain what integration should be used for.

--- recipes/xeriom/stages/integration.rb
+++ recipes/xeriom/stages/integration.rb
@@ -1,7 +1,9 @@
 # These are the default settings for a integration environment.
 # You can overwrite the hosts used in your own config/stages/integration.rb
 #
-# The integration environment is used by the continuous integration builder.
+# The integration environment is used to test systems integration. It should
+# be a clone of production except linked up to test datasources so destructive
+# actions won't affect live data.
 #
 role :web, "#{dns_name}-integration.#{domain}"
 role :app, "#{dns_name}-integration.#{domain}"
