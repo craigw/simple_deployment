c0bd99d3b841836f823bc30263f191b5173f932e
Craig R Webster <craig@barkingiguana.com>
2008-01-18 08:55:41 +0000

    Make it clear that staging should be used for User Acceptance Testing.

--- recipes/xeriom/stages/staging.rb
+++ recipes/xeriom/stages/staging.rb
@@ -1,7 +1,9 @@
 # These are the default settings for a staging environment.
 # You can overwrite the hosts used in your own config/stages/staging.rb
 #
-# Staging is used by QA to perform acceptance testing.
+# Staging is used by QA to perform user acceptance testing. It should be a 
+# clone of production except linked up to test datasources so destructive 
+# actions won't affect live data.
 #
 role :web, "#{dns_name}-staging.#{domain}"
 role :app, "#{dns_name}-staging.#{domain}"
