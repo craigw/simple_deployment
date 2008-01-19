17d6cec80c54f08ad926824bcfe9952d3d712918
Craig R Webster <craig@barkingiguana.com>
2008-01-19 11:29:21 +0000

    Pull in Monit and don't install the RI or RDoc to save time.

--- recipes/xeriom/gems.rb
+++ recipes/xeriom/gems.rb
@@ -1,9 +1,9 @@
 # TODO: Make sure Mongrel and MySQL gems get installed.
-set :required_gems, [ :mongrel_cluster ]
+set :required_gems, [ :mongrel_cluster, :monit ]
 set :gem_command, "gem"
 set :gem_install_command, "install"
 set :gem_prefix, ""
-set :gem_options, "-y"
+set :gem_options, "-y --no-ri --no-rdoc"
 
 namespace :gems do
   task :configure, :roles => [ :app ] do
