ead91ed3792b62f536e492c80d9c6ae6770d76ba
Craig R Webster <craig@barkingiguana.com>
2008-01-21 23:27:44 +0000

    Add a simple way to produce dynamically expandably multi-collection-select forms.

--- lib/xeriom/form/labelling_form_builder.rb
+++ lib/xeriom/form/labelling_form_builder.rb
@@ -25,9 +25,63 @@ module Xeriom
         CODE
         class_eval xeriom_form_tag_helper, __FILE__, __LINE__
       end
-      
+
+      # TODO: Document this.
+      #
+      # Produces a dynamically expandable list of <select> tags.
+      #
+      def select_many_from_list(attribute, collection, require_one_selection = true, value_method = :id, text_method = :name)
+        fieldset_name = "#{object_name}_#{attribute.to_s.pluralize}"
+        selected_objects = object.send(attribute)
+        html = %Q(<fieldset id="#{fieldset_name}">)
+        html << %Q(<legend>#{attribute.to_s.humanize}</legend>)
+        if selected_objects.empty?
+          html << selection_without_remove_link(attribute, collection, value_method, text_method)
+        else
+          selected_objects.each_with_index do |selected_object, index|
+            if index == 0 && require_one_selection
+              html << selection_without_remove_link(attribute, collection, value_method, text_method, selected_object)
+            else
+              html << selection_with_remove_link(attribute, collection, value_method, text_method, selected_object)
+            end
+          end
+        end
+
+        html +=<<-HTML
+        <script type="text/javascript">
+        var insertSelectionWithRemoveLink = function(element) {
+          element.up().insert({ before: '#{selection_with_remove_link(attribute, collection, value_method, text_method)}' });
+        }
+        </script>
+        <div>
+          <a href="#" onclick="javascript:insertSelectionWithRemoveLink($(this)); return false">+</a>
+        </div></fieldset>
+        HTML
+      end
+
       def submit_tag
         %Q(<input type="submit" value="#{object.new_record? ? "Create" : "Update" }" />)
+      end
+
+      private
+      def selection_without_remove_link(attribute, collection, value_method, text_method, selected_object)
+        selection = %Q(<div>#{build_selection_for_multiple_collection_select(attribute, collection, value_method, text_method, selected_object)}</div>)
+      end
+
+      def selection_with_remove_link(attribute, collection, value_method, text_method, selected_object = nil)
+        selection = %Q(<div>#{build_selection_for_multiple_collection_select(attribute, collection, value_method, text_method, selected_object)}<a href="#" onclick="javascript:$(this).up().remove(); return false">-</a></div>)
+      end
+
+      def build_selection_for_multiple_collection_select(attribute, collection, value_method, text_method, selected_object)
+        selection = %Q(<select name="#{object_name}[#{attribute.to_s.singularize}_ids][]">)
+        collection.each do |option|
+          selection << %Q(<option value="#{option.id}")
+          if !selected_object.blank? && option.send(value_method) == selected_object.send(value_method)
+            selection << %Q( selected="selected")
+          end
+          selection << %Q(>#{option.send(text_method)}</option>)
+        end
+        selection << %Q(</select>)
       end
     end
   end
